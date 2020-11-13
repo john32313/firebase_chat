0894118c8463ef5a7c5203a38f8c5b3f9c803c8d
lgiacalo <lgiacalo@student.42.fr>
2020-11-13 12:05:17 +0100

    add function createConversation(userList)

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import firebase from 'firebase/app';
 import {
   subscribeContactsAction,
   unsubscribeContactsAction,
@@ -8,12 +9,42 @@ import {
 } from '../store/actions';
 import { userSelector } from '../store/selectors';
 
+const createConversation = (userList) => {
+  const db = firebase.database();
+  const newConvRef = db.ref('conversations').push();
+  const {
+    path: {
+      pieces_: [, uidConv],
+    },
+  } = newConvRef;
+
+  newConvRef.set({
+    userList,
+    messages: [],
+  });
+
+  userList.forEach((curr) => {
+    db.ref(`conversationsList/${curr}/${uidConv}`).set({
+      userList: userList.filter((u) => u !== curr),
+      unread: 0,
+    });
+  });
+  return uidConv;
+};
+
 const HomePage = () => {
   const user = useSelector(userSelector);
   const dispatch = useDispatch();
 
   const handleClick = () => {
     console.log('Test : ');
+
+    const userList = [
+      user.uid,
+      '9NbBc6U8wqe2w1zwTeVTVoIQM5s1',
+      'x96kiiQ87RSwux1mi0eoGTwegwY2',
+    ];
+    createConversation(userList);
   };
 
   useEffect(() => {
