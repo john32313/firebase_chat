8dba8c2280ee62871b72b9dc79c1d96c0acb96d2
lgiacalo <lgiacalo@student.42.fr>
2020-11-13 18:36:28 +0100

    move subscribe contacts/conversationsList into MessagesPage component

--- src/components/ConversationList.jsx
+++ src/components/ConversationList.jsx
@@ -1,10 +1,6 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import {
-  subscribeContactsAction,
-  subscribeConversationsList,
-  unsubscribeContactsAction,
-} from '../store/actions';
+import React from 'react';
+import { useSelector } from 'react-redux';
+
 import {
   contactsSelector,
   conversationsListSelector,
@@ -16,17 +12,6 @@ function ConversationList() {
   const user = useSelector(userSelector);
   const contacts = useSelector(contactsSelector);
   const conversationsList = useSelector(conversationsListSelector);
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(subscribeContactsAction());
-    dispatch(subscribeConversationsList());
-
-    return () => {
-      dispatch(unsubscribeContactsAction());
-    };
-  }, []);
 
   return (
     <ul className="divide-y-2 divide-opacity-50 divide-red-600 bg-red-200 border-r-2 border-red-600 w-full md:w-1/3 lg:w-1/4 xl:w-1/5 h-screen overflow-y-auto p-2">
--- src/views/MessagesPage.jsx
+++ src/views/MessagesPage.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route } from 'react-router-dom';
-
+import { useDispatch } from 'react-redux';
 import Messages from '../components/Messages';
 import ConversationList from '../components/ConversationList';
+import {
+  subscribeContactsAction,
+  subscribeConversationsList,
+  unsubscribeContactsAction,
+  unsubscribeConversationsList,
+} from '../store/actions';
 
+// maybe rename this page/component ?
 function MessagesPage() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(subscribeContactsAction());
+    dispatch(subscribeConversationsList());
+
+    return () => {
+      dispatch(unsubscribeContactsAction());
+      dispatch(unsubscribeConversationsList());
+    };
+  }, []);
+
   return (
     <main className="flex">
       <ConversationList />
