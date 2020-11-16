3dd74cd7a8c9fb9fb7bf87f93794147c7e87c4b1
Corentin Crébier <crebier.corentin@gmail.com>
2020-11-16 10:38:50 +0100

    Add material-ui

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import './firebase';
 import App from './App';
 import store from './store/store';
 
 ReactDOM.render(
   <React.StrictMode>
+    <CssBaseline />
     <Provider store={store}>
       <App />
     </Provider>
